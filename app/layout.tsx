420deacbbf07a34c28aa925e1ebca45270889fc2
yogicmile <yogicmile@gmail.com>
2025-09-29 17:49:12 +0530

    Update to Latest Logo File

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,9 +54,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy.png" sizes="180x180" />
-        <link rel="shortcut icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy.png" />
+        <link rel="icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy copy.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy copy.png" sizes="180x180" />
+        <link rel="shortcut icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy copy.png" />
         <meta name="theme-color" content="#3B82F6" />
       </head>
       <body className={inter.className}>{children}</body>
