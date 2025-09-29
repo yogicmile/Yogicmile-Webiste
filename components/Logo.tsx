45913a0093883c70e147c18d360e32dcf3ea5057
yogicmile <yogicmile@gmail.com>
2025-09-29 20:56:09 +0530

    Actually Fix Logo Display Issue

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import React from 'react';
-import Image from 'next/image';
 
 interface LogoProps {
   size?: 'sm' | 'md' | 'lg';
@@ -17,40 +14,16 @@ export default function Logo({
   className = '' 
 }: LogoProps) {
   const sizeClasses = {
-    sm: variant === 'horizontal' ? 'h-8' : 'w-8 h-8',
-    md: variant === 'horizontal' ? 'h-10' : 'w-10 h-10',
-    lg: variant === 'horizontal' ? 'h-12' : 'w-12 h-12'
+    sm: 'h-8',
+    md: 'h-10', 
+    lg: 'h-12'
   };
-
-  const widthClasses = {
-    sm: 'w-32',
-    md: 'w-40',
-    lg: 'w-48'
-  };
-
-  if (variant === 'icon-only') {
-    return (
-      <div className={`${sizeClasses[size]} relative ${className}`}>
-        <Image
-          src="/yogic logo.png"
-          alt="YogicMile"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
-    );
-  }
 
   return (
-    <div className={`${sizeClasses[size]} ${widthClasses[size]} relative ${className}`}>
-      <Image
-        src="/yogic logo.png"
-        alt="YogicMile - Walk More. Earn More."
-        fill
-        className="object-contain"
-        priority
-      />
-    </div>
+    <img
+      src="/yogic logo.png"
+      alt="YogicMile"
+      className={`${sizeClasses[size]} w-auto object-contain ${className}`}
+    />
   );
 }
