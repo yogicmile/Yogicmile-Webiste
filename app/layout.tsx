4348d0ca1d67ed426fe198d24c1d948e8b07ab26
yogicmile <yogicmile@gmail.com>
2025-09-29 18:14:18 +0530

    Fix Webpack Cache and Favicon Errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" href="/image copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy.png" />
         <meta name="theme-color" content="#3B82F6" />
       </head>
